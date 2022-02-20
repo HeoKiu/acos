04b2e0e75eb4f8d3e1bb04b0a15eec52dbb978ff
Phan Ba Duc <47948344+HeoKiu@users.noreply.github.com>
2022-02-21 00:38:06 +0300

    Update main.c

--- inf15-2_posix_pipe_process-gcc-output-2/main.c
+++ inf15-2_posix_pipe_process-gcc-output-2/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 int main(int argc, char **argv) {
     if (argc < 2) {
         fprintf(stderr, "Too few arguments\n");
@@ -11,19 +10,15 @@ int main(int argc, char **argv) {
     }
     char *program_name = argv[1];
     int program_name_len = (int) strlen(program_name);
-
     int pipes[2];
     pipe(pipes);
-
     pid_t pid = fork();
     if (-1 == pid) {
         perror("Fork failed");
         return 1;
     }
-
     int read_fd = pipes[0];
     int write_fd = pipes[1];
-
     if (pid == 0) {
         close(read_fd);
         dup2(write_fd, 2);
@@ -32,14 +27,11 @@ int main(int argc, char **argv) {
     } else {
         close(write_fd);
         FILE *gcc_err = fdopen(read_fd, "r");
-
         int error_lines = 0;
         int warning_lines = 0;
-
         char *line = NULL;
         size_t len;
         int str_no, last_err_no = -1, last_warn_no = -1;
-
         while (getline(&line, &len, gcc_err) != -1) {
             if (strncmp(line, program_name, program_name_len) != 0) {
                 continue;
@@ -49,9 +41,8 @@ int main(int argc, char **argv) {
             if(str_no == 0){
                 continue;
             }
-            // + 1 skip ':'
             strtol(line_ptr + 1, &line_ptr, 10);
-            line_ptr += 2; // 2 symbols ':' and ' '
+            line_ptr += 2;
             if(strncmp(line_ptr, "error", 5) == 0){
                 error_lines += str_no != last_err_no;
                 last_err_no = str_no;
@@ -61,9 +52,8 @@ int main(int argc, char **argv) {
                 last_warn_no = str_no;
             }
         }
-
-        printf("%d %d\n", error_lines, warning_lines);
+        printf("Unique lines with warnings: %ld\n"
+               "Unique lines with errors: %ld\n", warning_lines, error_lines);
     }
-
     return 0;
 }
